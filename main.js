aa727042c55f1bd5b40901fcc99f40201c20055d
niveditha10 <80515547+annanya154@users.noreply.github.com>
2021-08-16 13:23:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+   poseNet=ml5.poseNet(video,modalLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function draw() {
+	game()
+}
+
+
+function modalLoaded()
+{
+console.log('modal is loaded');	
+}
+
+function gotPoses()
+{
+if(results.length>0)
+{
+console.log(results);
+noseX = results[0].pose.nose.x;
+noseY = results[0].pose.nose.y;
+}	
+}	
+
+
+
+
